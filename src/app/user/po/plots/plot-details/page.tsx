58b218357a010ba4b8178f3308eae6af39750ada
yobahBertrandYonkou <62288787+yobahBertrandYonkou@users.noreply.github.com>
2023-08-22 09:20:20 +0530

    new: Carousel added to plot details page

--- src/app/user/po/plots/plot-details/page.tsx
+++ src/app/user/po/plots/plot-details/page.tsx
@@ -1,4 +1,8 @@
+'use client';
+
 import plotDetailsCSS from "./plot-details.module.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 export default function PlotDetails() {
   return (
@@ -20,7 +24,15 @@ export default function PlotDetails() {
 
         {/* Photo carousel */}
         <div className={ plotDetailsCSS.photoCarouselContainer }>
-
+        <Carousel showArrows={true} autoPlay={true} interval={5} showThumbs={false}>
+        {/* <Carousel showArrows={true} > */}
+          <div className={plotDetailsCSS.imageContainer}>
+            <img className={plotDetailsCSS.images} src="https://images.freeimages.com/images/large-previews/636/holding-a-dot-com-iii-1411477.jpg" alt="image1" />
+          </div>
+          <div className={plotDetailsCSS.imageContainer}>
+            <img className={plotDetailsCSS.images} src="https://images.freeimages.com/images/large-previews/d4f/www-1242368.jpg" alt="image2" />
+          </div>
+        </Carousel>
         </div>
 
         {/* Plot description */}
