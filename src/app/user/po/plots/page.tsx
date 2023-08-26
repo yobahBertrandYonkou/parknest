641dc0703a4eefd11151c5b4527ecb054ec5b633
yobahBertrandYonkou <62288787+yobahBertrandYonkou@users.noreply.github.com>
2023-08-26 19:16:26 +0530

    new: Added warning box when deleting plot

--- src/app/user/po/plots/page.tsx
+++ src/app/user/po/plots/page.tsx
@@ -1,6 +1,11 @@
+"use client";
+import { useState } from "react";
 import plotCSS from "./plots.module.css";
 
 export default function POPlotsPage() {
+  // Variables for controlling delete warning pop up
+  const [showDeleteWarning, setShowDeleteWarning] = useState(false);
+
   return (
     <>
       <div className={plotCSS.mainContainer}>
@@ -8,9 +13,9 @@ export default function POPlotsPage() {
         <div className={plotCSS.actionBar}>
           <div className={plotCSS.searchContainer}>
             <div className={plotCSS.searchIcon}>
-            <div><i
-              className={`fa-solid fa-magnifying-glass`}
-            ></i></div>
+              <div>
+                <i className={`fa-solid fa-magnifying-glass`}></i>
+              </div>
             </div>
             <input
               className={plotCSS.search}
@@ -23,162 +28,192 @@ export default function POPlotsPage() {
           </div>
         </div>
         {/* Top bar ends */}
-        </div>
+      </div>
 
-        {/* Cards section */}
-        <div className="container-fluid">
-            <div className="row">
-
-                {/* Plot card */}
-                <div className="col-lg-3">
-                    <div className={ plotCSS.card }>
-                        <div className={ plotCSS.photo}></div>
-                        <div className={ plotCSS.cardContent}>
-                            <div className={ plotCSS.name}>Land title will be kept here</div>
-                        <div className={ plotCSS.description}>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        </div>
-                        <div className={ plotCSS.footer}>
-                            <div className={ plotCSS.price }>₹50 per hour</div>
-                            <div className={ plotCSS.icons }>
-                            <div className={ plotCSS.edit }>
-                            <i className="fa-solid fa-pen-to-square"></i>
-                            </div>
-                            <div className={ plotCSS.delete }>
-                            <i className="fa-solid fa-trash"></i>
-                            </div>
-                            
-                            </div>
-                        </div>
-                        </div>
-                    </div>
+      {/* Cards section */}
+      <div className="container-fluid">
+        <div className="row">
+          {/* Plot card */}
+          <div className="col-lg-3">
+            <div className={plotCSS.card}>
+              <div className={plotCSS.photo}></div>
+              <div className={plotCSS.cardContent}>
+                <div className={plotCSS.name}>Land title will be kept here</div>
+                <div className={plotCSS.description}>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.
                 </div>
-
-                {/* Plot card */}
-                <div className="col-lg-3">
-                    <div className={ plotCSS.card }>
-                        <div className={ plotCSS.photo}></div>
-                        <div className={ plotCSS.cardContent}>
-                            <div className={ plotCSS.name}>Land title will be kept here</div>
-                        <div className={ plotCSS.description}>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        </div>
-                        <div className={ plotCSS.footer}>
-                            <div className={ plotCSS.price }>₹50 per hour</div>
-                            <div className={ plotCSS.icons }>
-                            <div className={ plotCSS.edit }>
-                            <i className="fa-solid fa-pen-to-square"></i>
-                            </div>
-                            <div className={ plotCSS.delete }>
-                            <i className="fa-solid fa-trash"></i>
-                            </div>
-                            
-                            </div>
-                        </div>
-                        </div>
+                <div className={plotCSS.footer}>
+                  <div className={plotCSS.price}>₹50 per hour</div>
+                  <div className={plotCSS.icons}>
+                    <div className={plotCSS.edit}>
+                      <i className="fa-solid fa-pen-to-square"></i>
                     </div>
-                </div>
-
-                {/* Plot card */}
-                <div className="col-lg-3">
-                    <div className={ plotCSS.card }>
-                        <div className={ plotCSS.photo}></div>
-                        <div className={ plotCSS.cardContent}>
-                            <div className={ plotCSS.name}>Land title will be kept here</div>
-                        <div className={ plotCSS.description}>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        </div>
-                        <div className={ plotCSS.footer}>
-                            <div className={ plotCSS.price }>₹50 per hour</div>
-                            <div className={ plotCSS.icons }>
-                            <div className={ plotCSS.edit }>
-                            <i className="fa-solid fa-pen-to-square"></i>
-                            </div>
-                            <div className={ plotCSS.delete }>
-                            <i className="fa-solid fa-trash"></i>
-                            </div>
-                            
-                            </div>
-                        </div>
-                        </div>
+                    <div className={plotCSS.delete} onClick={() => setShowDeleteWarning(true)}>
+                      <i className="fa-solid fa-trash"></i>
                     </div>
+                  </div>
                 </div>
-                {/* Plot card */}
-                <div className="col-lg-3">
-                    <div className={ plotCSS.card }>
-                        <div className={ plotCSS.photo}></div>
-                        <div className={ plotCSS.cardContent}>
-                            <div className={ plotCSS.name}>Land title will be kept here</div>
-                        <div className={ plotCSS.description}>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        </div>
-                        <div className={ plotCSS.footer}>
-                            <div className={ plotCSS.price }>₹50 per hour</div>
-                            <div className={ plotCSS.icons }>
-                            <div className={ plotCSS.edit }>
-                            <i className="fa-solid fa-pen-to-square"></i>
-                            </div>
-                            <div className={ plotCSS.delete }>
-                            <i className="fa-solid fa-trash"></i>
-                            </div>
-                            
-                            </div>
-                        </div>
-                        </div>
-                    </div>
-                </div>
-                {/* Plot card */}
-                <div className="col-lg-3">
-                    <div className={ plotCSS.card }>
-                        <div className={ plotCSS.photo}></div>
-                        <div className={ plotCSS.cardContent}>
-                            <div className={ plotCSS.name}>Land title will be kept here</div>
-                        <div className={ plotCSS.description}>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        </div>
-                        <div className={ plotCSS.footer}>
-                            <div className={ plotCSS.price }>₹50 per hour</div>
-                            <div className={ plotCSS.icons }>
-                            <div className={ plotCSS.edit }>
-                            <i className="fa-solid fa-pen-to-square"></i>
-                            </div>
-                            <div className={ plotCSS.delete }>
-                            <i className="fa-solid fa-trash"></i>
-                            </div>
-                            
-                            </div>
-                        </div>
-                        </div>
-                    </div>
-                </div>
-                
-                {/* Plot card */}
-                <div className="col-lg-3">
-                    <div className={ plotCSS.card }>
-                        <div className={ plotCSS.photo}></div>
-                        <div className={ plotCSS.cardContent}>
-                            <div className={ plotCSS.name}>Land title will be kept here</div>
-                        <div className={ plotCSS.description}>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        </div>
-                        <div className={ plotCSS.footer}>
-                            <div className={ plotCSS.price }>₹50 per hour</div>
-                            <div className={ plotCSS.icons }>
-                            <div className={ plotCSS.edit }>
-                            <i className="fa-solid fa-pen-to-square"></i>
-                            </div>
-                            <div className={ plotCSS.delete }>
-                            <i className="fa-solid fa-trash"></i>
-                            </div>
-                            
-                            </div>
-                        </div>
-                        </div>
-                    </div>
-                </div>
-                
+              </div>
             </div>
+          </div>
+
+          {/* Plot card */}
+          <div className="col-lg-3">
+            <div className={plotCSS.card}>
+              <div className={plotCSS.photo}></div>
+              <div className={plotCSS.cardContent}>
+                <div className={plotCSS.name}>Land title will be kept here</div>
+                <div className={plotCSS.description}>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.
+                </div>
+                <div className={plotCSS.footer}>
+                  <div className={plotCSS.price}>₹50 per hour</div>
+                  <div className={plotCSS.icons}>
+                    <div className={plotCSS.edit}>
+                      <i className="fa-solid fa-pen-to-square"></i>
+                    </div>
+                    <div className={plotCSS.delete} onClick={() => setShowDeleteWarning(true)}>
+                      <i className="fa-solid fa-trash"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Plot card */}
+          <div className="col-lg-3">
+            <div className={plotCSS.card}>
+              <div className={plotCSS.photo}></div>
+              <div className={plotCSS.cardContent}>
+                <div className={plotCSS.name}>Land title will be kept here</div>
+                <div className={plotCSS.description}>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.
+                </div>
+                <div className={plotCSS.footer}>
+                  <div className={plotCSS.price}>₹50 per hour</div>
+                  <div className={plotCSS.icons}>
+                    <div className={plotCSS.edit}>
+                      <i className="fa-solid fa-pen-to-square"></i>
+                    </div>
+                    <div className={plotCSS.delete} onClick={() => setShowDeleteWarning(true)}>
+                      <i className="fa-solid fa-trash"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Plot card */}
+          <div className="col-lg-3">
+            <div className={plotCSS.card}>
+              <div className={plotCSS.photo}></div>
+              <div className={plotCSS.cardContent}>
+                <div className={plotCSS.name}>Land title will be kept here</div>
+                <div className={plotCSS.description}>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.
+                </div>
+                <div className={plotCSS.footer}>
+                  <div className={plotCSS.price}>₹50 per hour</div>
+                  <div className={plotCSS.icons}>
+                    <div className={plotCSS.edit}>
+                      <i className="fa-solid fa-pen-to-square"></i>
+                    </div>
+                    <div className={plotCSS.delete} onClick={() => setShowDeleteWarning(true)}>
+                      <i className="fa-solid fa-trash"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Plot card */}
+          <div className="col-lg-3">
+            <div className={plotCSS.card}>
+              <div className={plotCSS.photo}></div>
+              <div className={plotCSS.cardContent}>
+                <div className={plotCSS.name}>Land title will be kept here</div>
+                <div className={plotCSS.description}>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.
+                </div>
+                <div className={plotCSS.footer}>
+                  <div className={plotCSS.price}>₹50 per hour</div>
+                  <div className={plotCSS.icons}>
+                    <div className={plotCSS.edit}>
+                      <i className="fa-solid fa-pen-to-square"></i>
+                    </div>
+                    <div className={plotCSS.delete} onClick={() => setShowDeleteWarning(true)}>
+                      <i className="fa-solid fa-trash"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Plot card */}
+          <div className="col-lg-3">
+            <div className={plotCSS.card}>
+              <div className={plotCSS.photo}></div>
+              <div className={plotCSS.cardContent}>
+                <div className={plotCSS.name}>Land title will be kept here</div>
+                <div className={plotCSS.description}>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.
+                </div>
+                <div className={plotCSS.footer}>
+                  <div className={plotCSS.price}>₹50 per hour</div>
+                  <div className={plotCSS.icons}>
+                    <div className={plotCSS.edit}>
+                      <i className="fa-solid fa-pen-to-square"></i>
+                    </div>
+                    <div className={plotCSS.delete} onClick={() => setShowDeleteWarning(true)}>
+                      <i className="fa-solid fa-trash"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
+
+      {/* Delete plot pop up */}
+      {showDeleteWarning && (
+        <div className={plotCSS.transparentBg}>
+          <div className={plotCSS.deleteContainer}>
+            <div className="container-fluid">
+              <div className="row">
+                {/* Title bar */}
+                <div className={`col-12 ${plotCSS.titleBar}`}>
+                  <div>
+                    <b>Confirm delete</b>
+                  </div>
+                </div>
+                <hr />
+
+                {/* Middle section */}
+                <div className={plotCSS.warningText}>
+                  Are you sure you want to delete Land title will be kept here.
+                  This action is irreversible.
+                </div>
+                {/* Action buttons */}
+                <div className={plotCSS.action}>
+                  <div className={plotCSS.cancel} onClick={() => setShowDeleteWarning(false)}>
+                    Cancel
+                  </div>
+                  <div className={plotCSS.deleteBtn}>Delete</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
